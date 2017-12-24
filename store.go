7e13feda428b5b129e52514443d8b06ef04084cb
Tyson Trautmann <tysont@gmail.com>
2017-12-24 18:30:35 +0000

    Adding some store functionality.

--- store.go
+++ store.go
@@ -3,5 +3,8 @@ package corduroy
 type Store interface {
 	Put(key string, value string)
 	Get(key string) string
+	GetKeys(first int, count int) []string
+	Delete(key string)
 	Contains(key string) bool
+	Size() int
 }
